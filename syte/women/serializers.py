cfd7f04dbf59c828baf70b5764c2d7c130c29d0b
Shearer2 <aleksej.dorofeev.00@mail.ru>
2024-03-15 23:20:15 +0300

    Создание сериализатора для обработки данных о знаменитых женщинах, создание контроллера для обработки post и get запросов сериализатора.

--- syte/women/serializers.py
+++ syte/women/serializers.py
@@ -1,14 +1,64 @@
 # Импортируем сериализаторы.
+import io
+
 from rest_framework import serializers
+from rest_framework.parsers import JSONParser
+from rest_framework.renderers import JSONRenderer
+
 from .models import Women
+
+'''
+class WomenModel:
+    def __init__(self, title, content):
+        self.title = title
+        self.content = content
+'''
 
 
 # Указываем сериализатор, который работает с моделями.
-class WomenSerializer(serializers.ModelSerializer):
+class WomenSerializer(serializers.Serializer):
     """Сериализатор для знаменитых женщин."""
 
+    # В сериализаиторе прописываем те же атрибуты, которые присутствуют и в классе WomenModel.
+    # Чтобы сериализатор знал, что данная переменная является обычной строкой, необходимо использовать класс CharField.
+    #title = serializers.CharField(max_length=255)
+    #content = serializers.CharField()
+    title = serializers.CharField(max_length=255)
+    content = serializers.CharField()
+    time_create = serializers.DateTimeField(read_only=True)
+    time_update = serializers.DateTimeField(read_only=True)
+    is_published = serializers.BooleanField(default=True)
+    cat_id = serializers.IntegerField()
+
+    '''
     class Meta:
         model = Women
         # Поля, которые будем использовать для сериализации, они будут отправляться обратно пользователю.
         fields = ('title', 'cat')
+    '''
 
+'''
+# Функция для преобразования объектов класса WomenModel в json-формат.
+def encode():
+    model = WomenModel('Angelina Jolie', 'Content: Angelina Jolie')
+    # Пропускаем созданный объект через сериализатор.
+    model_sr = WomenSerializer(model)
+    # Объект сериализатор выводит полученные данные в виде data.
+    print(model_sr.data, type(model_sr.data), sep='\n')
+    # Представляем объект сериализтор в виде json строки.
+    json = JSONRenderer().render(model_sr.data)
+    print(json)
+
+
+# Преобразование из json строки обратно.
+def decode():
+    # Имитируем поступление запроса от клиента.
+    stream = io.BytesIO(b'{"title": "Angelina Jolie", "content": "Content: Angelina Jolie"}')
+    # Для формирования словаря используем JSONParser.
+    data = JSONParser().parse(stream)
+    # Чтобы сериализатор декодировал данные необъодимо использовать параметр data.
+    serializer = WomenSerializer(data=data)
+    # Проверяем корректность данных.
+    serializer.is_valid()
+    print(serializer.validated_data)
+'''
--- syte/women/views.py
+++ syte/women/views.py
@@ -1,12 +1,43 @@
+from django.forms import model_to_dict
 from django.shortcuts import render
 # Импортируем generics
 from rest_framework import generics
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
 from .models import Women
 from .serializers import WomenSerializer
 
 
-class WomenAPIView(generics.ListAPIView):
+# Класс APIView является базовым классом django rest framework.
+class WomenAPIView(APIView):
+    # Метод get отвечает за get-запросы поступающие на сервер.
+    def get(self, request):
+        # Чтобы не возникла ошибка необходимо указать метод values().
+        w = Women.objects.all()
+        # Возвращаем клиенту, от которого пришёл запрос, json строку.
+        # Чтобы сериализатор обрабатывал не одну запись, а список записей, указываем параметр mane=True.
+        return Response({'posts': WomenSerializer(w, many=True).data})
+
+    #Добавляем определённые данные в базу данных.
+    def post(self, request):
+        # Чтобы не возникали ошибки при вводе неверных данных необходимо сначала создать сериализатор
+        # с полученными данными.
+        serializer = WomenSerializer(data=request.data)
+        # Если не было указано какое-то поле, то генерируем исключение.
+        serializer.is_valid(raise_exception=True)
+
+        post_new = Women.objects.create(
+            title=request.data['title'],
+            content=request.data['content'],
+            cat_id=request.data['cat_id']
+        )
+        # Функция model_to_dict преобразует объект django в словарь.
+        return Response({'post': WomenSerializer(post_new).data})
+
+
+'''class WomenAPIView(generics.ListAPIView):
     queryset = Women.objects.all()
     # Указываем класс сериализатор.
     serializer_class = WomenSerializer
-
+'''
